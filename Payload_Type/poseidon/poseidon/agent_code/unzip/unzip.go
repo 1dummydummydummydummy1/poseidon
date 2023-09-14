0d609b470dd4e83c2fc112e09dc33548c9570ad8
1dummydummydummydummy1 <144202200+1dummydummydummydummy1@users.noreply.github.com>
2023-09-14 12:56:00 -0400

    Create unzip.go

---
+++ Payload_Type/poseidon/poseidon/agent_code/unzip/unzip.go
@@ -0,0 +1,86 @@
+package unzip
+
+import (
+  "io"
+  "path/filepath"
+  "os"
+  "archive/zip"
+
+	"github.com/MythicAgents/poseidon/Payload_Type/poseidon/agent_code/pkg/utils/structs"
+)
+
+type Arguments struct {
+	StringTest      string `json:"zipfile"`
+	StringTest      string `json:"destination"`
+}
+
+func Run(task structs.Task) {
+	msg := structs.Response{}
+	msg.TaskID = task.TaskID
+
+	var args Arguments
+	err := json.Unmarshal([]byte(task.Params), &args)
+	if err != nil {
+		msg.UserOutput = err.Error()
+		msg.Completed = true
+		msg.Status = "error"
+		task.Job.SendResponses <- msg
+		return
+	}
+
+	if Unzip()==nil{
+		msg.UserOutput = "unzip completed successfully"
+		msg.Completed = true
+		task.Job.SendResponses <- msg
+	} else {
+		msg.UserOutput = "failed to unzip"
+		msg.Completed = true
+		task.Job.SendResponses <- msg
+	}
+
+}
+
+
+func Unzip(source, destination string) error {
+	archive, err := zip.OpenReader(source)
+	if err != nil {
+		return err
+	}
+	defer archive.Close()
+	for _, file := range archive.Reader.File {
+		reader, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer reader.Close()
+		path := filepath.Join(destination, file.Name)
+		// Remove file if it already exists; no problem if it doesn't; other cases can error out below
+		_ = os.Remove(path)
+		// Create a directory at path, including parents
+		err = os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			return err
+		}
+		// If file is _supposed_ to be a directory, we're done
+		if file.FileInfo().IsDir() {
+			continue
+		}
+		// otherwise, remove that directory (_not_ including parents)
+		err = os.Remove(path)
+		if err != nil {
+			return err
+		}
+		// and create the actual file.  This ensures that the parent directories exist!
+		// An archive may have a single file with a nested path, rather than a file for each parent dir
+		writer, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+		defer writer.Close()
+		_, err = io.Copy(writer, reader)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
